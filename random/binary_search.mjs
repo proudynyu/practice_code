8b4fdb8ac1c23e018708e524cf6fa0fb741f7365
Igor Becker <igor_nyu@hotmail.com>
2023-04-18 15:45:32 -0300

    feat: comparing processing time

--- random/binary_search.mjs
+++ random/binary_search.mjs
@@ -14,4 +14,18 @@ function binarySearch(arr, target, start, end) {
 
 const arr = [2,5,8,12,16,23,38,56,72,91]
 
+console.time('binary')
 console.log(binarySearch(arr, 91, 0, arr.length - 1))
+console.timeEnd('binary')
+
+
+console.time('index of')
+console.log(arr.indexOf(91))
+console.timeEnd('index of')
+
+
+console.time('for loop')
+  for(let i = 0; i < arr.length; i++) {
+    if (arr[i] === 91) console.log(i)
+  }
+console.timeEnd('for loop')
